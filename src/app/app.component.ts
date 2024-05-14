2abd44798bea9a5d44ab9e76bad45110356d2af5
unknown <arix69sex@gmail.com>
2024-05-14 15:36:15 -0500

    feat: Implement navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,51 @@
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from './services/authService';
 import { ProductService } from './services/productService';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbModule, HttpClientModule],
+  imports: [RouterOutlet, NgbModule, HttpClientModule, CommonModule, RouterModule],
   providers: [AuthService, ProductService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-angular';
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  isLoggedIn(): boolean {
+    // Check if the user is logged in based on localStorage JWT
+    const token = localStorage.getItem('token');
+    console.log("is logged in", !!token)
+    return !!token; // Returns true if jwt exists, false otherwise
+  }
+
+  getUsernameFromJWT(): string {
+    const token = this.authService.getToken()
+    const payload = JSON.parse(atob(token!.split('.')[1]));
+    return payload.username;
+  }
+
+  routeToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  routeToSignup(): void {
+    this.router.navigate(['/signup']);
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-//import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'edit-product/:id', component: EditProductComponent  },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
   ];
